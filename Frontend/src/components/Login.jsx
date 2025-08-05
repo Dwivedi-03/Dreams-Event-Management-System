8146650bc4fd27fe9f19d3e03c8e51cbcbbea94d
Dwivedi <devweb910@gmail.com>
2025-08-05 22:01:35 +0530

    Handle submit in login component

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,6 +1,10 @@
 // import { FaUser, FaLock } from "react-icons/fa";
 
 function Login() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
   return (
     <>
       {/* <section className="h-[90vh] flex justify-center items-center">
@@ -51,55 +55,76 @@ function Login() {
           </div>
         </div>
       </section> */}
-      <main
-        className="h-screen flex justify-center items-center bg-no-repeat bg-center bg-cover bg-[url('https://images.pexels.com/photos/2306281/pexels-photo-2306281.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')]">
+      <main className="h-screen flex justify-center items-center bg-no-repeat bg-center bg-cover bg-[url('https://images.pexels.com/photos/2306281/pexels-photo-2306281.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')]">
         <div className="h-auto w-[80%] lg:w-[50%] bg-[#ffedd5]/45 rounded">
-            <div className="h-[20%] flex justify-center items-center font-[cursive] text-2xl">Login</div>
-            <div className="h-[70%]">
-                <div className="px-2 md:px-4 py-4">
-                    <div className="relative">
-                        <input type="text" name="username" id="username"
-                            className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 border-0 border-b-2 border-[#431407] appearance-none focus:outline-none focus:ring-0 focus:border-slate-600 peer"
-                            placeholder=" " />
-                        <label htmlFor="username"
-                            className="absolute text-sm text-gray-900 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-slate-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Username</label>
-                    </div>
+          <div className="h-[20%] flex justify-center items-center font-[cursive] text-2xl">
+            Login
+          </div>
+          <div className="h-[70%]">
+            <form onSubmit={handleSubmit}>
+              <div className="px-2 md:px-4 py-4">
+                <div className="relative">
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 border-0 border-b-2 border-[#431407] appearance-none focus:outline-none focus:ring-0 focus:border-slate-600 peer"
+                    placeholder=" "
+                  />
+                  <label
+                    htmlFor="username"
+                    className="absolute text-sm text-gray-900 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-slate-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+                  >
+                    Username
+                  </label>
                 </div>
-                <div className="px-2 md:px-4 py-2 lg:py-1">
-                    <div className="relative">
-                        <input type="text" name="password" id="password"
-                            className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 border-0 border-b-2 border-[#431407] appearance-none focus:outline-none focus:ring-0 focus:border-slate-600 peer"
-                            placeholder=" " />
-                        <label htmlFor="password"
-                            className="absolute text-sm text-gray-900 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-slate-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Password</label>
-                    </div>
+              </div>
+              <div className="px-2 md:px-4 py-2 lg:py-1">
+                <div className="relative">
+                  <input
+                    type="text"
+                    name="password"
+                    id="password"
+                    className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 border-0 border-b-2 border-[#431407] appearance-none focus:outline-none focus:ring-0 focus:border-slate-600 peer"
+                    placeholder=" "
+                  />
+                  <label
+                    htmlFor="password"
+                    className="absolute text-sm text-gray-900 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-slate-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+                  >
+                    Password
+                  </label>
                 </div>
-                <div className="px-2 md:px-4 flex justify-evenly items-center">
-                    <div>
-                        <input type="radio" name="userType" id="manager" />
-                        <label htmlFor="manager">Manager</label>
-                    </div>
-                    <div>
-                        <input type="radio" name="userType" id="user" />
-                        <label htmlFor="user">User</label>
-                    </div>
+              </div>
+              <div className="px-2 md:px-4 flex justify-evenly items-center">
+                <div>
+                  <input type="radio" name="userType" id="manager" />
+                  <label htmlFor="manager">Manager</label>
                 </div>
-                <div className="px-2 md:px-4 py-4 flex justify-center items-center">
-                    <button className="bg-[#431407] px-6 py-2 rounded text-[#ffedd5] font-[cursive]">Login</button>
+                <div>
+                  <input type="radio" name="userType" id="user" />
+                  <label htmlFor="user">User</label>
                 </div>
-                <div className="px-2 md:px-4 py-2 flex justify-between items-center">
-                    <p className="text-sm">
-                        <input type="checkbox" name="rememberMe" id="rememberMe" />
-                        <label htmlFor="rememberMe">remember me</label>
-                    </p>
-                    <p className="text-sm">Forgot password</p>
-                </div>
-            </div>
-            <div className="h-[10%] pb-2 flex justify-center items-center font-mono text-sm text-blue-800 italic">
-                create an account
-            </div>
+              </div>
+              <div className="px-2 md:px-4 py-4 flex justify-center items-center">
+                <button className="bg-[#431407] px-6 py-2 rounded text-[#ffedd5] font-[cursive]">
+                  Login
+                </button>
+              </div>
+              <div className="px-2 md:px-4 py-2 flex justify-between items-center">
+                <p className="text-sm">
+                  <input type="checkbox" name="rememberMe" id="rememberMe" />
+                  <label htmlFor="rememberMe">remember me</label>
+                </p>
+                <p className="text-sm">Forgot password</p>
+              </div>
+            </form>
+          </div>
+          <div className="h-[10%] pb-2 flex justify-center items-center font-mono text-sm text-blue-800 italic">
+            create an account
+          </div>
         </div>
-    </main>
+      </main>
     </>
   );
 }
